f7b592f5df19cbe9e2edf80bb483abf9f6cf6d5a
tneulinger <neulingert@gmail.com>
2020-04-06 15:42:27 +0200

    added README

---
+++ build/CMakeFiles/CASCADE.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CASCADE.dir/CASCADE.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADEActionInitialization.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADEDetectorConstruction.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADEEventAction.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADEPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADERunAction.cc.o"
+  "CMakeFiles/CASCADE.dir/src/CASCADESteppingAction.cc.o"
+  "CASCADE.pdb"
+  "CASCADE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CASCADE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
